a1f9b9ddbfaa9ab5087f6c3e59bd72bbfe8bec30
Jesse Williams <jesse@negativespacesounds.com>
2024-03-06 03:30:03 +1100

    update: v0.0.19

--- Formula/excavator.rb
+++ Formula/excavator.rb
@@ -1,8 +1,8 @@
 class Excavator < Formula
   desc "Tui application for managing and re-exporting audio samples"
   homepage "https://github.com/jesses-code-adventures/excavator"
-  url "https://github.com/jesses-code-adventures/excavator/archive/refs/tags/v0.0.18-lw.tar.gz"
-  sha256 "105c429a51a7214af1c54b713ef2c6c9c0979c185f15472f461f3353dfbb4fa8"
+  url "https://github.com/jesses-code-adventures/excavator/archive/refs/tags/v0.0.19-lw.tar.gz"
+  sha256 "9bf184d4493ff71c3e1d7764fbe8a23a6a00c88f0e6637721fcb83da1cacc112"
   license "MIT"
 
   depends_on "go" => :build
