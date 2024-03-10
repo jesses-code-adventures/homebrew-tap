01161e0c37cffc7d7df0c17923bce0d1154ba267
~ jesse ~ <113159758+jesses-code-adventures@users.noreply.github.com>
2024-03-11 00:42:03 +1100

    Update excavator.rb

--- Formula/excavator.rb
+++ Formula/excavator.rb
@@ -5,6 +5,12 @@ class Excavator < Formula
   sha256 "5b9ec39dd372e380da85309312076128dc246ebb794681643235aeddf2b87b8b"
   license "MIT"
 
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
   test do
     system "#{bin}/excavator", "--version"
   end
