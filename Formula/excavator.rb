f03d370ec7de62f2b5afff1eeb516d8ced314a21
Jesse Williams <jesse@negativespacesounds.com>
2024-03-10 23:32:03 +1100

    fix: remove blank line

--- Formula/excavator.rb
+++ Formula/excavator.rb
@@ -5,7 +5,6 @@ class Excavator < Formula
   sha256 "59a82f0e8eb2efc282603cc3c98896cff85fe44cb98987b3578d9150c3c5ecff"
   license "MIT"
 
-
   test do
     system "#{bin}/excavator", "--version"
   end
