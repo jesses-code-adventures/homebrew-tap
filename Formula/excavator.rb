eb0aa8dbc42070c8a2294b88d9f7d6c6640b4134
~ jesse ~ <113159758+jesses-code-adventures@users.noreply.github.com>
2024-03-11 00:32:11 +1100

    Update excavator.rb

--- Formula/excavator.rb
+++ Formula/excavator.rb
@@ -1,7 +1,7 @@
 class Excavator < Formula
   desc "Tui application for managing and re-exporting audio samples"
   homepage "https://github.com/jesses-code-adventures/excavator"
-  url "https://github.com/jesses-code-adventures/excavator/archive/refs/tags/v0.0.48-lw.tar.gz"
+  url "https://github.com/jesses-code-adventures/excavator/archive/refs/tags/0.0.48-lw.tar.gz"
   sha256 "59a82f0e8eb2efc282603cc3c98896cff85fe44cb98987b3578d9150c3c5ecff"
   license "MIT"
 
